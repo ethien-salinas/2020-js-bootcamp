314c29809f0976f3fbb21fafbdd6462c35ae71dc
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-07-04 16:04:14 -0500

    [add] jest with babel and test cases

---
+++ sesion03/jest-and-es6/src/__test__/calc.test.js
@@ -0,0 +1,25 @@
+import { sum, diff } from '../calc'
+import sumTestCases from './calc.sum.testcases.json'
+
+test('3 + 6 should be 9', () => {
+  expect(sum(3, 6)).toBe(9)
+})
+
+test('3 + 6 should not be 6', () => {
+  expect(sum(3, 6)).not.toBe(6)
+})
+
+test('3 - 6 should be -3', () => {
+  expect(diff(3, 6)).toBe(-3)
+})
+
+
+for (let i = 0; i < sumTestCases.length; i++) {
+
+  test(`${sumTestCases[i].number1} + ${sumTestCases[i].number2} should be ${sumTestCases[i].expect}`, () => {
+    expect(
+      sum(sumTestCases[i].number1, sumTestCases[i].number2)
+    ).toBe(sumTestCases[i].expect)
+  })
+
+}
---
+++ sesion03/jest-and-es6/src/index.js
@@ -0,0 +1,5 @@
+const calc = require('./calc')
+
+const res = calc.suma(3,6)
+
+console.log(res)
