e8afcd3151bd5f83df41d361ff8f273898c665f4
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-07-11 08:57:19 -0500

    [update] es6 import syntax

--- sesion03/jest-and-es6/src/index.js
+++ sesion03/jest-and-es6/src/index.js
@@ -1,7 +1,5 @@
-const calc = require('./calc')
-// import { sum } from './calc'
+import { sum } from './calc'
 
-const res = calc.sum(3,6)
-// const res = sum(3, 6)
+const res = sum(3, 6)
 
 console.log(res)
