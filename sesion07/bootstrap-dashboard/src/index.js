6a18570a5767952c2c16899424d11f4d7bdd2b6e
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-08-01 15:48:34 -0500

    [add] admin base template

---
+++ sesion07/bootstrap-dashboard/src/index.js
@@ -0,0 +1,7 @@
+import './sass/styles.scss'
+import '@fortawesome/fontawesome-free/js/all'
+import 'chart.js'
+import './js/dashboard-bar-chart'
+
+const sayHello = 'bootstrap dashboard'
+console.log(sayHello)
