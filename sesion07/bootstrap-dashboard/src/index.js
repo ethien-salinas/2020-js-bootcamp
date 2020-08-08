4a7fff7757bb24f7bf34cbfe46fcda0590b4423a
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-08-08 08:56:55 -0500

    [add] doughnut chart and progress bars

--- sesion07/bootstrap-dashboard/src/index.js
+++ sesion07/bootstrap-dashboard/src/index.js
@@ -2,6 +2,7 @@ import './sass/styles.scss'
 import '@fortawesome/fontawesome-free/js/all'
 import 'chart.js'
 import './js/dashboard-bar-chart'
+import './js/dashboard-doughnut-chart'
 
 const sayHello = 'bootstrap dashboard'
 console.log(sayHello)
