820d532391e0c2ede8bf46a7a18f3566c3d4817d
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-08-29 10:07:15 -0500

    [add] use of components

--- sesion10/react-admin/src/view/Dashboard.js
+++ sesion10/react-admin/src/view/Dashboard.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import Component1 from '../components/Component1'
+import Component2 from '../components/Component2'
+import Component3 from '../components/Component3'
 
 const Dashboard = () => {
-  return <h3>Dashboard view</h3>
+
+  return (
+    <>
+      <h3>Dashboard view</h3>
+      <Component1 />
+      <Component2 />
+      <Component3 />
+    </>
+  )
 }
 
 export default Dashboard
