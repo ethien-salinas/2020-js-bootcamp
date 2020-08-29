77c6e3eff4b663aa092a1727a4f0989977dc66c0
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-08-29 12:27:10 -0500

    [add] path param

--- sesion10/react-admin/src/router/index.js
+++ sesion10/react-admin/src/router/index.js
@@ -8,6 +8,8 @@ import Home from '../view/Home'
 import Login from '../view/Login'
 import Dashboard from '../view/Dashboard'
 import PrivateRoute from './PrivateRoute'
+import Users from '../view/Users'
+import User from '../view/User'
 
 const MainRouter = () => {
   return (
@@ -15,6 +17,8 @@ const MainRouter = () => {
       <Nav />
       <section className="container">
         <Route exact path="/" component={Home} />
+        <Route exact path="/user" component={Users} />
+        <Route exact path="/user/:userId" component={User} />
         <PrivateRoute path="/dashboard">
           <Dashboard />
         </PrivateRoute>
