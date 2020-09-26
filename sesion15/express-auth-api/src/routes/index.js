7e3986c90946a433a30ebc67a06d99bc44b59073
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-09-26 14:18:48 -0500

    [add] save register in DB

--- sesion15/express-auth-api/src/routes/index.js
+++ sesion15/express-auth-api/src/routes/index.js
@@ -9,14 +9,24 @@ router.post('/login', (req, res) => {
   res.send('[POST] /login')
 })
 router.post('/register', (req, res) => {
-  res.send(JSON.stringify({
-    msg: '[POST] /register',
-    values: [
-      req.body.name,
-      req.body.email,
-      bcrypt.hashSync(req.body.password, 9),
-    ]
-  }))
+  db.insert([
+    req.body.name,
+    req.body.email,
+    bcrypt.hashSync(req.body.password, 9),
+  ],
+    (err) => {
+      if (err)
+        return res.status(500).send({
+          success: false,
+          msg: `there was a problem registering the user ${req.body.email}`,
+          err
+        })
+      res.status(200).send({
+        success: true,
+        user: [req.body.name, req.body.email]
+      })
+    }
+  )
 })
 
 module.exports = router
