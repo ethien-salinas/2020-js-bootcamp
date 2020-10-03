ada8cd37fbd2debc09620bef8841905aeda85080
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-10-03 12:19:32 -0500

    [add] function call and declaration

--- sesion16/go-hello-world/hello.go
+++ sesion16/go-hello-world/hello.go
@@ -2,11 +2,38 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Println("Hello World!")
-	fmt.Println(quote.Go())
+
+	// declarando variable antes de usarla
+	var result string
+	result = getQuote("glass")
+	fmt.Println(result)
+
+	// usando duck typing
+	goCallResult := getQuote("go")
+	fmt.Println(goCallResult)
+
+	fmt.Println(getQuote("hello"))
+	fmt.Println(getQuote("opt"))
+}
+
+func getQuote(kindOfQuote string) string {
+	var result string
+	switch strings.ToLower(kindOfQuote) {
+	case "glass":
+		result = quote.Glass()
+	case "go":
+		result = quote.Go()
+	case "hello":
+		result = quote.Hello()
+	case "opt":
+		result = quote.Opt()
+	}
+	return result
 }
