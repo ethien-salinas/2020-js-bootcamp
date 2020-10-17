8d0e02c46c1203bd79dee0b3f278aae823f8100b
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-10-17 16:33:58 -0500

    [add] babel and sequelize

--- sesion17/api-base-tech/src/index.js
+++ sesion17/api-base-tech/src/index.js
@@ -1,7 +1,40 @@
 require('dotenv').config()
 // require('./dotenv')
 const logger = require('./logger')
-require('./bcrypt')
+// require('./bcrypt')
+const bcrypt = require('bcrypt')
+const connection = require('./sequelize-orm')
+
+const store = connection()
+
+const tryStore = async () => {
+  try {
+    // const users = await store.User.findAll();
+    // console.log(users);
+    // // insertar
+    // const user = await store.User.create({
+    //   name: 'Ethien Salinas',
+    //   email: 'ethien.salinas@gmail.com',
+    //   password: 'qwerty'
+    // })
+    // update
+    // await store.User.update({ password: await bcrypt.hash('qwerty', 9) }, {
+    //   where: {
+    //     email: 'ethien.salinas@gmail.com'
+    //   }
+    // })
+    //nueva consulta
+    const ethienUser = await store.User.findOne({
+      where: {
+        name: 'Ethien Salinas'
+      }
+    })
+    console.log(`password match: ${await bcrypt.compare('qwerty', ethienUser.password)}`)
+  } catch (error) {
+    logger.error(error)
+  }
+}
+tryStore()
 
 // const constValue = {
 //     name: 'Ethien',
