1aeaee62c44f3354f8080dd835c9d54ca8fcd6aa
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-11-02 14:16:14 -0600

    [add] jwt protection to routes

--- sesion19/graphql-complete-api/src/resolver/index.ts
+++ sesion19/graphql-complete-api/src/resolver/index.ts
@@ -7,15 +7,15 @@ const authAPI: AuthAPI = new AuthAPI();
 export const resolvers = {
   Query: {
     login: (_, { email, password }) => authAPI.getToken({ email, password }),
-    users: async (_, __, { token }) => {
-      return authAPI.verifyToken(token) && userController.getUsers()
-    },
-    user: (_, { id }) => userController.getUser(id),
+    users: (_, __, { token }) => authAPI.verifyToken(token)
+      && userController.getUsers(),
+    user: (_, { id }, { token }) => authAPI.verifyToken(token)
+      && userController.getUser(id),
   },
   Mutation: {
     saveUser: (_, { name, lastName, email, password, isAdmin }) =>
       userController.saveUser({ name, lastName, email, password, isAdmin }),
-    updateUser: (_, { id, name, lastName, email, password, isAdmin }) =>
-      userController.updateUser({ id, name, lastName, email, password, isAdmin }),
+    updateUser: (_, { id, name, lastName, email, password, isAdmin }, { token }) => authAPI.verifyToken(token)
+      && userController.updateUser({ id, name, lastName, email, password, isAdmin }),
   },
 };
