fcc8b84199818ed7a00126c68b350b5dec6209e7
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-11-03 01:18:07 -0600

    [refactor] userController to userAPI

--- sesion19/graphql-complete-api/src/resolver/index.ts
+++ sesion19/graphql-complete-api/src/resolver/index.ts
@@ -1,21 +1,21 @@
 import { AuthAPI } from "../controller/AuthAPI";
 import { UserAPI } from "../controller/UserAPI";
 
-const userController: UserAPI = new UserAPI();
+const userAPI: UserAPI = new UserAPI();
 const authAPI: AuthAPI = new AuthAPI();
 
 export const resolvers = {
   Query: {
     login: (_, { email, password }) => authAPI.getToken({ email, password }),
     users: (_, __, { token }) => authAPI.verifyToken(token)
-      && userController.getUsers(),
+      && userAPI.getUsers(),
     user: (_, { id }, { token }) => authAPI.verifyToken(token)
-      && userController.getUser(id),
+      && userAPI.getUser(id),
   },
   Mutation: {
     saveUser: (_, { name, lastName, email, password, isAdmin }) =>
-      userController.saveUser({ name, lastName, email, password, isAdmin }),
+      userAPI.saveUser({ name, lastName, email, password, isAdmin }),
     updateUser: (_, { id, name, lastName, email, password, isAdmin }, { token }) => authAPI.verifyToken(token)
-      && userController.updateUser({ id, name, lastName, email, password, isAdmin }),
+      && userAPI.updateUser({ id, name, lastName, email, password, isAdmin }),
   },
 };
