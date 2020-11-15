aedd09ae5553151c0fcf69892f558ca18672118e
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-11-15 12:58:42 -0600

    [added] PrivateRoute and ProvideAuth bases

--- sesion20-22/blog-final-project/blog-frontend/src/view/App.jsx
+++ sesion20-22/blog-final-project/blog-frontend/src/view/App.jsx
@@ -11,33 +11,36 @@ import { LoginPage } from './LoginPage';
 import { ProtectedPage } from './ProtectedPage';
 import { About } from './About';
 import { Users } from './Users';
+import { PrivateRoute } from '../components/PrivateRoute';
+import { ProvideAuth } from '../hook/ProvideAuth';
 
 export const App = () => {
   return (
-    <Router>
-      <div>
+    <ProvideAuth>
+      <Router>
+        <div>
 
-        <nav>
-          <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/users">Users</Link></li>
-            <li><Link to="/public">Public Page</Link></li>
-            <li><Link to="/protected">Protected Page</Link></li>
-          </ul>
-        </nav>
+          <nav>
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/users">Users</Link></li>
+              <li><Link to="/public">Public Page</Link></li>
+              <li><Link to="/protected">Protected Page</Link></li>
+            </ul>
+          </nav>
 
-        <Switch>
-          <Route path="/public"><PublicPage /></Route>
-          <Route path="/login"><LoginPage /></Route>
-          {/* <PrivateRoute path="/protected"><ProtectedPage /></PrivateRoute> */}
-          <Route path="/protected"><ProtectedPage /></Route>
-          <Route path="/about"><About /></Route>
-          <Route path="/users"><Users /></Route>
-          <Route path="/"><Home /></Route>
-        </Switch>
+          <Switch>
+            <Route path="/public"><PublicPage /></Route>
+            <Route path="/login"><LoginPage /></Route>
+            <PrivateRoute path="/protected"><ProtectedPage /></PrivateRoute>
+            <Route path="/about"><About /></Route>
+            <Route path="/users"><Users /></Route>
+            <Route path="/"><Home /></Route>
+          </Switch>
 
-      </div>
-    </Router>
+        </div>
+      </Router>
+    </ProvideAuth>
   );
 }
