fa28ebe111138eb4db072d481c632628d638e4cc
Ethien Salinas <ethien@Ethiens-MacBook-Pro.local>
2020-11-21 10:16:02 -0600

    [fix] provider path

--- sesion20-22/blog-final-project/blog-frontend/src/page/App.jsx
+++ sesion20-22/blog-final-project/blog-frontend/src/page/App.jsx
@@ -1,6 +1,6 @@
 import '../css/App.css';
 import { BrowserRouter as Router } from "react-router-dom";
-import { ProvideAuth } from '../hook/ProvideAuth';
+import { ProvideAuth } from '../hook/authHook';
 import { Navbar } from '../components/Navbar';
 import { Routes } from '../routes/Routes';
 
